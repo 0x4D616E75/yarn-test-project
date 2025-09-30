a54c337e6b5ca5690b6d8de987b9d9b4dfc56b20
0x4D616E75 <0x4d616e75@elektronische-nachricht.de>
2025-09-30 21:11:20 +0200

    fix 500 error again without additional exception filter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
